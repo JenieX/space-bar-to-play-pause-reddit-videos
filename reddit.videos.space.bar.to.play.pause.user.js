a45caefd005762aa97ea4571529eeb69a92cb257
FlowerForWar <102888858+FlowerForWar@users.noreply.github.com>
2022-05-17 03:53:59 +0300

    auto-focus an auto-playing video

--- reddit.videos.space.bar.to.play.pause.user.js
+++ reddit.videos.space.bar.to.play.pause.user.js
@@ -1,16 +1,31 @@
 // ==UserScript==
 // @name           Reddit Videos - [Space Bar] to Play/Pause
 // @namespace      https://github.com/FlowerForWar/Reddit-Videos-Space-Bar-to-Play-Pause
-// @description    Play/Pause a focused video by pressing the [Space Bar]
-// @version        0.04
+// @description    Play/Pause a focused video by pressing the [Space Bar]. Automatically focus an auto-playing video
+// @version        0.05
 // @author         FlowrForWar
 // @match          https://www.reddit.com/*
 // @grant          none
+// @compatible     edge Tampermonkey or Violentmonkey
+// @compatible     firefox Greasemonkey, Tampermonkey or Violentmonkey
+// @compatible     chrome Tampermonkey or Violentmonkey
+// @compatible     opera Tampermonkey or Violentmonkey
 // @supportURL     https://github.com/FlowerForWar/Reddit-Videos-Space-Bar-to-Play-Pause/issues
 // @license        MIT
 // ==/UserScript==
 
-let fullscreenElement;
+/*
+
+## Some notes on how it works
+* Video losing focus, means the default behavior of Space Bar is activated, that is scrolling down
+* Video loses focus in these situations
+  * When the video is finished playing
+  * When the video is auto-paused by scrolling down
+  * When the video is paused by click and this script has been triggerd at least once on that video
+
+*/
+
+let fullscreenElement, pausedByScript;
 document.documentElement.addEventListener('fullscreenchange', () => {
 	const fullscreen_had_video = !!fullscreenElement && !!fullscreenElement.querySelector(':scope > video');
 	if (fullscreen_had_video) fullscreenElement.closest('[id^="t3_"]').focus();
@@ -18,9 +33,18 @@ document.documentElement.addEventListener('fullscreenchange', () => {
 });
 window.addEventListener('keydown', keydown);
 
+// https://stackoverflow.com/a/31133401
+Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+	get: function() {
+		return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+	},
+});
+
 async function keydown(event) {
 	const { code: key, shiftKey } = event;
 	if (key !== 'Space' && key !== 'ArrowLeft' && key !== 'ArrowRight') return;
+	const playing_video = [...document.getElementsByTagName('video')].filter(video => video.playing)[0];
+	if (playing_video) playing_video.closest('[id^="t3_"]').focus();
 	if (!(document.activeElement && document.activeElement.id.startsWith('t3_')) && !fullscreenElement) return;
 	const video = fullscreenElement ? fullscreenElement.querySelector(':scope > video') : document.activeElement.getElementsByTagName('video')[0];
 	if (!video) return;
@@ -30,8 +54,12 @@ async function keydown(event) {
 			if (shiftKey) {
 				video.currentTime = 0;
 				video.play();
-			} else if (video.paused) video.play();
-			else video.pause();
+			} else if (video.paused) {
+				video.play();
+			} else {
+				pausedByScript = !0;
+				video.pause();
+			}
 			break;
 		case 'ArrowLeft':
 			video.currentTime -= shiftKey ? 10 : 2;
@@ -40,5 +68,19 @@ async function keydown(event) {
 			video.currentTime += shiftKey ? 10 : 2;
 			break;
 	}
+	video.addEventListener('pause', pause);
+	video.addEventListener('play', play);
 	video.dispatchEvent(new MouseEvent('mousemove', { bubbles: !0 }));
+}
+
+function pause() {
+	if (pausedByScript) return;
+	// console.warn('Lost focus');
+	document.activeElement.blur();
+	// this.removeEventListener('pause', pause);
+}
+
+function play() {
+	pausedByScript = !1;
+	// this.removeEventListener('play', play);
 }
